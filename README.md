10c89055cef832d9784260b36cfb663e4173646c
CaptainAyal <ayalmesher@gmail.com>
2025-03-27 12:31:40 +0200

    initial setup

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Ruby-TicTacToe
+
+Build a tic-tac-toe game on the command line where two human players can play against each other and the board is displayed in between turns.
+
+Add computer player ability if desired
---
+++ TicTacToe.rb
@@ -0,0 +1,10 @@
+# TicTacToe
+# Create Board Class
+#   Board can be displayed
+#   Board can change to show player choices
+# Create Win condition Class
+#   Win condition can check for win
+#   Trigger Win statement
+# Create Player Class
+#   Player can choose move
+#   Player can hold previous moves
