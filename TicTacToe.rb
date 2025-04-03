54ab0525a3ace886bdbe47b54aa3ba14d9aace41
CaptainAyal <ayalmesher@gmail.com>
2025-04-03 23:07:02 +0300

    Game plays 1 turn each. Has logic to check for improper moves. Does not check win condition

--- TicTacToe.rb
+++ TicTacToe.rb
@@ -1,6 +1,7 @@
 # TicTacToe
 require_relative 'lib/board'
 require_relative 'lib/Win_condition'
+require_relative 'lib/Player'
 
 # Create Board Class
 #   Board can be displayed
@@ -17,3 +18,45 @@ game_board.display_board
 
 win = Win_condition.new
 puts win
+
+player_one = Player.new(1, 'x')
+player_two = Player.new(2, 'o')
+
+current_player = if current_player == player_one
+  player_two
+else
+  player_one
+end
+
+choice = current_player.move
+if game_board.spaces.include?(choice)
+  game_board.spaces = game_board.spaces.map do |e|
+    if e == choice
+      current_player.symbol
+    else
+      e
+    end
+  end
+else puts 'that is not a valid move'
+end
+game_board.display_board
+
+current_player = if current_player == player_one
+  player_two
+else
+  player_one
+end
+
+choice = current_player.move
+if game_board.spaces.include?(choice)
+  game_board.spaces = game_board.spaces.map do |e|
+    if e == choice
+      current_player.symbol
+    else
+      e
+    end
+  end
+else puts 'that is not a valid move'
+end
+game_board.display_board
+
--- lib/Player.rb
+++ lib/Player.rb
@@ -1,15 +1,18 @@
 # Player
 
 class Player
-  attr_accessor :moves, :player_num
-  def initialize (num)
+  attr_accessor :moves, :player_num, :symbol
+
+  def initialize(num, sym)
     @player_num = num
     @moves = []
+    @symbol = sym
   end
 
   def move
     puts "Player number #{player_num}. Please enter your move."
     choice = gets.to_i
     moves.push(choice)
+    choice
   end
 end
